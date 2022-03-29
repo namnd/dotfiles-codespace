22aa895b52d4ae6f773b42e1bda51eeb7b6adcd7
Nam Nguyen <nam.nguyen@indebted.co>
2022-03-30 10:19:43 +1100

    install tailscale

--- install.sh
+++ install.sh
@@ -5,15 +5,18 @@ sh <(curl -L https://nixos.org/nix/install) --no-daemon
 sleep 1
 . /home/codespace/.nix-profile/etc/profile.d/nix.sh
 
-# Install tools
-nix-env -iA \
-  nixpkgs.fzf \
-  nixpkgs.jq \
-  nixpkgs.go \
-  nixpkgs.gopls \
-  nixpkgs.nodejs \
-  nixpkgs.ripgrep \
-  nixpkgs.tailscale \
-  nixpkgs.tmux \
-  nixpkgs.trash-cli \
-  nixpkgs.tree-sitter
+# Install tailscale
+curl -fsSL https://tailscale.com/install.sh | sh
+
+# # Install tools
+# nix-env -iA \
+#   nixpkgs.fzf \
+#   nixpkgs.jq \
+#   nixpkgs.go \
+#   nixpkgs.gopls \
+#   nixpkgs.nodejs \
+#   nixpkgs.ripgrep \
+#   nixpkgs.tailscale \
+#   nixpkgs.tmux \
+#   nixpkgs.trash-cli \
+#   nixpkgs.tree-sitter
