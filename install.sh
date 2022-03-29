4c8dcda99706150dc1c7f181c5e69c460d437add
Nam Nguyen <nam.nguyen@indebted.co>
2022-03-30 00:59:58 +1100

    initial commit

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Install nix
+sh <(curl -L https://nixos.org/nix/install) --no-daemon
+. /home/codespace/.nix-profile/etc/profile.d/nix.sh
+
+
+# Install tools
+nix-env -iA \
+  nixpkgs.fzf \
+  nixpkgs.jq \
+  nixpkgs.go \
+  nixpkgs.gopls \
+  nixpkgs.nodejs \
+  nixpkgs.ripgrep \
+  nixpkgs.tailscale \
+  nixpkgs.tmux \
+  nixpkgs.trash-cli \
+  nixpkgs.tree-sitter
