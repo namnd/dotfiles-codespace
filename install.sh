054010e3700b7f6b0f3453ad63535594ef896703
Nam Nguyen <nam.nguyen@indebted.co>
2022-03-31 14:15:36 +1100

    install fzf from source

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ sudo add-apt-repository ppa:neovim-ppa/stable
 
  # Install tools
 sudo apt install -y \
-  fzf \
   jq \
   ripgrep \
   neovim \
@@ -16,9 +15,14 @@ sudo apt install -y \
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
+# Install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --no-fish --no-zsh --key-bindings --completion
+
 # Install gopls (go language server protocol)
 go install golang.org/x/tools/gopls@latest
 
 source=/workspaces/.codespaces/.persistedshare/dotfiles
 ln -s $source/tmux.conf $HOME/.tmux.conf
 ln -s $source/nvim $HOME/.config/nvim
+ln -s $source/gitconfig $HOME/.gitconfig
