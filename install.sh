f8bcd18c4ad920c7d7a69e856f38b72ced1fc84a
Nam Nguyen <nam.nguyen@indebted.co>
2022-04-05 09:56:25 +1000

    install terraform-ls

--- install.sh
+++ install.sh
@@ -43,6 +43,12 @@ terraform_version='0.12.25'
 tfenv install ${terraform_version}
 tfenv use ${terraform_version}
 
+# Install terraform-ls
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt install terraform-ls -y
+
 # Symlinks
 source=/workspaces/.codespaces/.persistedshare/dotfiles
 ln -s $source/tmux.conf $HOME/.tmux.conf
